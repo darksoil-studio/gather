54e1a5073f7b0dce1f7e63a5be55585c726d6bf4
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-01-31 21:36:35 +0100

    Upgrade WIP

--- ui/src/holochain-app.ts
+++ ui/src/holochain-app.ts
@@ -1,118 +1,121 @@
-import '@webcomponents/scoped-custom-element-registry';
+import "@webcomponents/scoped-custom-element-registry";
 
-import { CellClient, HolochainClient } from '@holochain-open-dev/cell-client';
 import {
   FileStorageClient,
   fileStorageClientContext,
-} from '@holochain-open-dev/file-storage';
+} from "@holochain-open-dev/file-storage";
 import {
   AgentAvatar,
   Profile,
+  ProfileListItemSkeleton,
   ProfilePrompt,
-  ProfilesService,
+  ProfilesClient,
   ProfilesStore,
   profilesStoreContext,
-} from '@holochain-open-dev/profiles';
+} from "@holochain-open-dev/profiles";
 import {
   ActionHash,
-  AppWebsocket,
-  InstalledAppInfo,
-  InstalledCell,
-} from '@holochain/client';
-import { contextProvider } from '@lit-labs/context';
-import { ScopedElementsMixin } from '@open-wc/scoped-elements';
+  AppAgentClient,
+  AppAgentWebsocket,
+} from "@holochain/client";
+import { provide } from "@lit-labs/context";
+import { ScopedElementsMixin } from "@open-wc/scoped-elements";
 import {
   CircularProgress,
   Fab,
   Icon,
   IconButton,
   TopAppBar,
-} from '@scoped-elements/material-web';
-import { SlBreadcrumb, SlBreadcrumbItem } from '@scoped-elements/shoelace';
-import { LitElement, css, html } from 'lit';
-import { StoreSubscriber, TaskSubscriber } from 'lit-svelte-stores';
-import { customElement, property, state } from 'lit/decorators.js';
+} from "@scoped-elements/material-web";
+import { LitElement, css, html } from "lit";
+import {
+  AsyncStatus,
+  StoreSubscriber,
+  TaskSubscriber,
+} from "@holochain-open-dev/stores";
+import { customElement, property, state } from "lit/decorators.js";
 
-import { gatherStoreContext } from './gather/gather/context';
-import { AllEvents } from './gather/gather/elements/all-events';
-import { AttendeesForEvent } from './gather/gather/elements/attendees-for-event';
-import { CreateEvent } from './gather/gather/elements/create-event';
-import { EventDetail } from './gather/gather/elements/event-detail';
-import { GatherClient } from './gather/gather/gather-client';
-import { GatherStore } from './gather/gather/gather-store';
-import { sharedStyles } from './shared-styles';
+import { gatherStoreContext } from "./gather/gather/context";
+import { AllEvents } from "./gather/gather/elements/all-events";
+import { AttendeesForEvent } from "./gather/gather/elements/attendees-for-event";
+import { CreateEvent } from "./gather/gather/elements/create-event";
+import { EventDetail } from "./gather/gather/elements/event-detail";
+import { GatherClient } from "./gather/gather/gather-client";
+import { GatherStore } from "./gather/gather/gather-store";
+import { localized, msg } from "@lit/localize";
+import { DisplayError, sharedStyles } from "@holochain-open-dev/elements";
 
 type View =
-  | { view: 'all_events' }
-  | { view: 'event_detail'; selectedEventHash: ActionHash }
-  | { view: 'create_event' };
+  | { view: "all_events" }
+  | { view: "event_detail"; selectedEventHash: ActionHash }
+  | { view: "create_event" };
 
+@localized()
 export class HolochainApp extends ScopedElementsMixin(LitElement) {
-  @contextProvider({ context: fileStorageClientContext })
+  @provide({ context: fileStorageClientContext })
   @property()
   _fileStorageClient!: FileStorageClient;
 
-  @contextProvider({ context: gatherStoreContext })
+  @provide({ context: gatherStoreContext })
   @property()
   _gatherStore!: GatherStore;
 
   @state() _loading = true;
 
-  @state() _view: View = { view: 'all_events' };
+  @state() _view: View = { view: "all_events" };
 
-  @contextProvider({ context: profilesStoreContext })
+  @provide({ context: profilesStoreContext })
   @property()
   _profilesStore!: ProfilesStore;
 
-  _myProfileTask!: TaskSubscriber<[], Profile | undefined>;
+  _myProfile!: StoreSubscriber<AsyncStatus<Profile | undefined>>;
+
+  _client!: AppAgentClient;
 
   async firstUpdated() {
-    const url = `ws://localhost:${process.env.HC_PORT}`;
+    this._client = await AppAgentWebsocket.connect("", "gather");
 
-    const appWebsocket = await AppWebsocket.connect(url);
-    const client = new HolochainClient(appWebsocket);
-
-    const appInfo = await appWebsocket.appInfo({
-      installed_app_id: 'gather',
-    });
-
-    const installedCells = appInfo.cell_data;
-    const profilesCell = installedCells[0] as InstalledCell;
-
-    const cellClient = new CellClient(client, profilesCell);
-
-    await this.initStores(cellClient);
+    await this.initStores(this._client);
 
     this._loading = false;
   }
 
-  async initStores(cellClient: CellClient) {
-    this._profilesStore = new ProfilesStore(new ProfilesService(cellClient));
-    this._gatherStore = new GatherStore(new GatherClient(cellClient));
-    this._fileStorageClient = new FileStorageClient(cellClient);
+  async initStores(appAgentClient: AppAgentClient) {
+    this._profilesStore = new ProfilesStore(
+      new ProfilesClient(appAgentClient, "gather")
+    );
+    this._gatherStore = new GatherStore(
+      new GatherClient(appAgentClient, "gather")
+    );
+    this._fileStorageClient = new FileStorageClient(appAgentClient, "gather");
   }
 
   renderMyProfile() {
-    return this._myProfileTask?.render({
-      pending: () => html``,
-      complete: profile =>
-        profile
-          ? html` <div
-              class="row"
-              style="align-items: center;"
-              slot="actionItems"
-            >
-              <agent-avatar
-                .agentPubKey=${this._profilesStore.myAgentPubKey}
-              ></agent-avatar>
-              <span style="margin: 0 16px;">${profile?.nickname}</span>
-            </div>`
-          : undefined,
-    });
+    switch (this._myProfile.value.status) {
+      case "pending":
+        return html`<profile-list-item-skeleton></profile-list-item-skeleton>`;
+      case "complete":
+        const profile = this._myProfile.value.value;
+        if (!profile) return html``;
+
+        return html`<div
+          class="row"
+          style="align-items: center;"
+          slot="actionItems"
+        >
+          <agent-avatar .agentPubKey=${this._client.myPubKey}></agent-avatar>
+          <span style="margin: 0 16px;">${profile?.nickname}</span>
+        </div>`;
+      case "error":
+        return html`<display-error
+          .error=${this._myProfile.value.error.data.data}
+          tooltip
+        ></display-error>`;
+    }
   }
 
   renderContent() {
-    if (this._view.view === 'event_detail')
+    if (this._view.view === "event_detail")
       return html`
         <div class="flex-scrollable-parent">
           <div class="flex-scrollable-container">
@@ -122,26 +125,26 @@ export class HolochainApp extends ScopedElementsMixin(LitElement) {
                   style="flex: 1; margin-right: 16px"
                   .eventHash=${this._view.selectedEventHash}
                   @event-deleted=${() => {
-          this._view = { view: 'all_events' };
-        }}
+                    this._view = { view: "all_events" };
+                  }}
                 ></event-detail>
               </div>
             </div>
           </div>
         </div>
       `;
-    if (this._view.view === 'create_event')
+    if (this._view.view === "create_event")
       return html` <div class="flex-scrollable-parent">
         <div class="flex-scrollable-container">
           <div class="flex-scrollable-y">
             <div class="column" style="flex: 1; align-items: center;">
               <create-event
                 @event-created=${(e: CustomEvent) => {
-          this._view = {
-            view: 'event_detail',
-            selectedEventHash: e.detail.eventHash,
-          };
-        }}
+                  this._view = {
+                    view: "event_detail",
+                    selectedEventHash: e.detail.eventHash,
+                  };
+                }}
                 style="margin-top: 16px"
               ></create-event>
             </div>
@@ -154,14 +157,16 @@ export class HolochainApp extends ScopedElementsMixin(LitElement) {
           <div class="flex-scrollable-y">
             <div class="column" style="flex: 1; align-items: center;">
               <div class="column" style="width: 800px;">
-                <span class="title" style="margin: 16px 0;">All Events</span>
+                <span class="title" style="margin: 16px 0;"
+                  >${msg("All Events")}</span
+                >
                 <all-events
                   @event-selected=${(e: CustomEvent) => {
-        this._view = {
-          view: 'event_detail',
-          selectedEventHash: e.detail.eventHash,
-        };
-      }}
+                    this._view = {
+                      view: "event_detail",
+                      selectedEventHash: e.detail.eventHash,
+                    };
+                  }}
                   style="flex: 1"
                 >
                 </all-events>
@@ -174,24 +179,24 @@ export class HolochainApp extends ScopedElementsMixin(LitElement) {
       <mwc-fab
         extended
         icon="add"
-        label="Create Event"
+        .label=${msg("Create Event")}
         @click=${() => {
-        this._view = { view: 'create_event' };
-      }}
+          this._view = { view: "create_event" };
+        }}
         style="position: absolute; right: 16px; bottom: 16px;"
       ></mwc-fab>`;
   }
 
   renderBackButton() {
-    if (this._view.view === 'all_events') return html``;
+    if (this._view.view === "all_events") return html``;
 
     return html`
       <mwc-icon-button
         icon="arrow_back"
         slot="navigationIcon"
         @click=${() => {
-        this._view = { view: 'all_events' };
-      }}
+          this._view = { view: "all_events" };
+        }}
       ></mwc-icon-button>
     `;
   }
@@ -210,10 +215,7 @@ export class HolochainApp extends ScopedElementsMixin(LitElement) {
         ${this.renderBackButton()}
         <div slot="title">Gather</div>
         <div class="fill row" style="width: 100vw; height: 100%;">
-          <profile-prompt
-            style="flex: 1;"
-            @profile-created=${() => this._myProfileTask.run()}
-          >
+          <profile-prompt style="flex: 1;">
             ${this.renderContent()}
           </profile-prompt>
         </div>
@@ -224,16 +226,18 @@ export class HolochainApp extends ScopedElementsMixin(LitElement) {
 
   static get scopedElements() {
     return {
-      'agent-avatar': AgentAvatar,
-      'profile-prompt': ProfilePrompt,
-      'mwc-icon-button': IconButton,
-      'mwc-top-app-bar': TopAppBar,
-      'mwc-circular-progress': CircularProgress,
-      'all-events': AllEvents,
-      'create-event': CreateEvent,
-      'mwc-icon': Icon,
-      'event-detail': EventDetail,
-      'mwc-fab': Fab,
+      "agent-avatar": AgentAvatar,
+      "profile-prompt": ProfilePrompt,
+      "mwc-icon-button": IconButton,
+      "mwc-top-app-bar": TopAppBar,
+      "mwc-circular-progress": CircularProgress,
+      "all-events": AllEvents,
+      "create-event": CreateEvent,
+      "mwc-icon": Icon,
+      "event-detail": EventDetail,
+      "mwc-fab": Fab,
+      "display-error": DisplayError,
+      "profile-list-item-skeleton": ProfileListItemSkeleton,
     };
   }
 
