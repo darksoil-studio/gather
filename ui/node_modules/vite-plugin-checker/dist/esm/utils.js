2928e3ffd7fcc81287c8dba01261af64237208c9
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-03-01 19:23:19 +0100

    Removed lock

--- ui/node_modules/vite-plugin-checker/dist/esm/utils.js
+++
@@ -1,8 +0,0 @@
-import { isMainThread as _isMainThread, threadId } from "worker_threads";
-const isInVitestEntryThread = threadId === 1 && process.env["VITEST"];
-const isMainThread = _isMainThread || isInVitestEntryThread;
-export {
-  isInVitestEntryThread,
-  isMainThread
-};
-//# sourceMappingURL=utils.js.map
