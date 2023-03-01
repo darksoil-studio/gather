82cd01eae254199dcc4430ab9b5ee458c15c64c0
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-03-01 20:30:27 +0100

    Package applet working

--- ui/applet/vite.config.ts
+++ ui/applet/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import checker from "vite-plugin-checker";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,14 +8,17 @@ export default defineConfig({
     checker({
       typescript: true,
       eslint: {
-        lintCommand: "eslint --ext .ts,.html . --ignore-path .gitignore",
+        lintCommand: 'eslint --ext .ts src --ignore-path .gitignore',
       },
     }),
   ],
   build: {
     lib: {
+      formats: ['es'],
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      fileName: 'index',
     },
   },
 });
