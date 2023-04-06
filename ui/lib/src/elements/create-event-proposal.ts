ff98d261a9ccddd8742d51b3026b03e197d8e626
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-04-06 19:11:33 +0200

    Fixed end time validation

--- ui/lib/src/elements/create-event-proposal.ts
+++ ui/lib/src/elements/create-event-proposal.ts
@@ -74,17 +74,28 @@ export class CreateEventProposal extends CreateCallToAction {
           >
             <sl-input
               name="start_time"
+              id="start-time"
               required
               .label=${msg('Start Time')}
+              @sl-input=${() => {
+                //@ts-ignore
+                this.requestUpdate();
+              }}
             ></sl-input
           ></lit-flatpickr>
           <lit-flatpickr
             .dateFormat=${'Y-m-d H:i'}
             .enableTime=${true}
             style="flex: 1"
+            .minDate=${new Date(
+              //@ts-ignore
+              (this.shadowRoot?.getElementById('start-time') as any)?.value
+            ).valueOf()}
           >
             <sl-input
               required
+              .disabled=${!//@ts-ignore
+              (this.shadowRoot?.getElementById('start-time') as any)?.value}
               name="end_time"
               .label=${msg('End Time')}
             ></sl-input
